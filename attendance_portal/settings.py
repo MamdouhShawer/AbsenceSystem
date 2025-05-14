f04e069c9e27c94735e46d0c133ed1618a8a01a3
MamdouhShawer <shawermamdouh4@gmail.com>
2025-05-14 17:44:40 +0100

    adding style file

--- attendance_portal/settings.py
+++ attendance_portal/settings.py
@@ -120,9 +120,8 @@ CSRF_TRUSTED_ORIGINS = [
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # Default primary key field type
