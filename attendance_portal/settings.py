d8aa50d8edcf972fddb5d0ea030e39c93eff4fd5
MamdouhShawer <shawermamdouh4@gmail.com>
2025-05-14 04:54:37 +0100

    edits

--- attendance_portal/settings.py
+++ attendance_portal/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-90wf$4@83t-#p&7sx=90l714v8wbonh@#l&#pd3z40nd!!qidg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.azurewebsites.net']
 
